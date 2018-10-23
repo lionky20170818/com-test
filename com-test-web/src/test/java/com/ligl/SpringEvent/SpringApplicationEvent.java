8970bacd9f9eb35ecde1a3beb31f68d1c2519ea9
liguoliang <liguoliang@xforceplus.com>
2018-10-23 15:42:13 +0800

    spring事件监听

---
+++ com-test-web/src/test/java/com/ligl/SpringEvent/SpringApplicationEvent.java
@@ -0,0 +1,11 @@
+package com.ligl.SpringEvent;
+
+/**
+ * Author: created by liguoliang
+ * Date: 2018/10/23 15:41 41
+ * Version: 1.0
+ * Function: 用于:Spring事件监听
+ */
+public class SpringApplicationEvent {
+
+}
